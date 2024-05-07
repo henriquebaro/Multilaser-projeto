4cf78b82dac67b52d2528b46f884fb1edf3732e4
julia caruso <juliacaruso779@gmail.com>
2024-05-07 16:11:55 -0300

    ju bug

--- frontend/src/Pages/produtos.js
+++ frontend/src/Pages/produtos.js
@@ -1,22 +1,15 @@
-import React from "react"
-import Produtos_form from "../Components/Produtos_Form"
-import Tabelaprodutos from "../Components/Produtos_Tabela";
-function Produtos(){
-    return (
-      <>
-        <div>
-          <h2>teste pagina products</h2>
-          <Produtos_form/>
-          <br>
-          </br>
-          <br>
-          </br>
-          <br>
-          </br>
-          <Tabelaprodutos/>
-        </div>
-      </>
-    );
-  };
-  
-  export default Produtos;
+import React from "react";
+
+
+
+
+function Produto() {
+
+  return (
+    <>
+     
+    </>
+  );
+}
+
+export default Produto;

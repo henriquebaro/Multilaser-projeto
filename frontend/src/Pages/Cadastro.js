7609c19eefe0d4f9e3cfb03a4554d14c114017e5
henriquebaro <henrique.barros@portalsesisp.org.br>
2024-05-07 13:49:41 -0300

    atualizaçao chakra

--- frontend/src/Pages/Cadastro.js
+++ frontend/src/Pages/Cadastro.js
@@ -2,12 +2,41 @@
 import React from "react";
 import CadastroForm from "../Components/CadastroForm";
 import TabelaCadastro from "../Components/TabelaCadastro";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react'
 const Cadastro = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <>
       <div>
         <h2>Página Cadastro</h2>
-        <CadastroForm />
+      
+        <Button onClick={onOpen}>Open Modal</Button>
+        <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody> 
+             <CadastroForm />
+           
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
         <TabelaCadastro />
       </div>
     </>

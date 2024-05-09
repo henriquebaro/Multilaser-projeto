168ddbc55f22f62ebf8a828ff91c306b8e65e346
henriquebaro <henrique.barros@portalsesisp.org.br>
2024-05-09 14:08:29 -0300

    chacra maldito

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 //Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { ChakraProvider } from "@chakra-ui/react";
 //Importar complementos aqui
 import Rotas from "./Routes";
 
@@ -11,13 +11,18 @@ import Navegacao from "./Components/Navegacao";
 
 const App = () => {
   return (
-    <>
-
+    <>     
+    
       <Router>
-        <Navegacao />
+
+ 
+   
+  <ChakraProvider>
+        <Navegacao />  
         <Rotas />
+       </ChakraProvider>
       </Router>    
-     
+      
     </>
   );
 };
--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,10 +1,10 @@
 // Home
 import React from "react";
-
+import { Button, ButtonGroup } from '@chakra-ui/react'
 function Home(){
   return (
     <>
-     
+     <Button colorScheme='blue'>Button</Button>
     </>
   );
 };

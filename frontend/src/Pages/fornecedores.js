aef9275a6ee48241bf3bbf71248f6c7462133f91
isanile <isabellaanilealves@gmail.com>
2024-05-14 11:01:22 -0300

    atualizacao forms fornecedores

--- frontend/src/Pages/fornecedores.js
+++ frontend/src/Pages/fornecedores.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Fornecedor_form from "../Components/Fornecedor_form";
 import TabelaClientes from "../Components/Fornecedor_tabela";
+import '../css/fornecedorform.css'
 
 const Fornecedores = () => {
   return (
     <>
       <div>
-        <h2>Fornecedores    </h2>
+        <center>
+          <h2 className="h2">FORNECEDORES </h2>
+        </center>
+        
     <Fornecedor_form/>
     <TabelaClientes/>
       </div>

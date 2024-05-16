3d98b2cd157e4df615b4835597f005b91ebb3c77
julia caruso <juliacaruso779@gmail.com>
2024-05-16 13:52:10 -0300

    navbar

--- frontend/src/Pages/Clientes.js
+++ frontend/src/Pages/Clientes.js
@@ -5,16 +5,21 @@ import '../css/cadastroclientes.css'
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { useState } from 'react';
+import '../css/navbar.css'
+
 
 const Clientes = () => {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+ 
   
   return (
     <>
       <div>
+
         <center>
           <h2 className="h2">CADASTRE-SE</h2>
         </center>
---
+++ frontend/src/Pages/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function OffcanvasExample() {
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default OffcanvasExample;

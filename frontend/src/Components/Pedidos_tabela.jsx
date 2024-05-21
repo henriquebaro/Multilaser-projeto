be60e477d897236e8b75376d8e10672904582d0a
henriquebaro <henrique.barros@portalsesisp.org.br>
2024-05-21 11:12:25 -0300

    pagina pedidos

--- frontend/src/Components/Pedidos_tabela.jsx
+++ frontend/src/Components/Pedidos_tabela.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
 
 function Tabela_pedidos() {
   const [slClientes, setSlClientes] = useState([]);
@@ -8,20 +9,42 @@ function Tabela_pedidos() {
   const [idClientela, setClienteId] = useState(null);
   const [idProducts, setProdutoId] = useState(null);
   const [pedidos, setPedidos] = useState([]);
+  const [clientes, setClientes] = useState({});
+  const [produtos, setProdutos] = useState({});
+  const [quantidadeDisponivel, setQuantidadeDisponivel] = useState(0);
+  const [quantidadeSelecionada, setQuantidadeSelecionada] = useState(0);
+  const [precoProduto, setPrecoProduto] = useState(0);
+  const [valorTotal, setValorTotal] = useState(0);
 
   const handleClienteChange = (event) => {
     setClienteId(event.target.value);
   };
 
   const handleProdutoChange = (event) => {
-    setProdutoId(event.target.value);
+    const produtoId = event.target.value;
+    setProdutoId(produtoId);
+
+    const produtoSelecionado = slProdutos.find(produto => produto.idProdutos === parseInt(produtoId));
+    if (produtoSelecionado) {
+      setQuantidadeDisponivel(produtoSelecionado.quantidade);
+      setPrecoProduto(produtoSelecionado.preco);
+    }
+  };
+
+  const handleQuantidadeChange = (event) => {
+    const quantidade = parseInt(event.target.value);
+    setQuantidadeSelecionada(quantidade);
+    setValorTotal(quantidade * precoProduto);
   };
 
   const handleAdicionarPedido = async () => {
+    const valorTotalPedido = quantidadeSelecionada * precoProduto;
     try {
       await axios.post('http://localhost:3001/pedidos', {
         id: idClientela,
-        idProdutos: idProducts
+        idProdutos: idProducts,
+        quantidade: quantidadeSelecionada,
+        valorTotal: valorTotalPedido
       });
       window.location.reload();
       alert('Pedido adicionado com sucesso');
@@ -29,6 +52,7 @@ function Tabela_pedidos() {
       console.error('Erro ao adicionar pedido:', error);
       alert('Erro ao adicionar pedido');
     }
+
   };
 
   useEffect(() => {
@@ -56,7 +80,23 @@ function Tabela_pedidos() {
 
     fetchData();
   }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get("http://localhost:3001/pedidos");
+        // Convertendo os valores para números
+        const pedidosFormatados = data.map(pedido => ({
+          ...pedido,
+          valorTotal: parseFloat(pedido.valorTotal)
+        }));
+        setPedidos(pedidosFormatados);
+      } catch (error) {
+        console.error("Erro ao buscar pedidos:", error);
+      }
+    };
 
+    fetchData();
+  }, []);
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -66,20 +106,68 @@ function Tabela_pedidos() {
         console.error("Erro ao buscar pedidos:", error);
       }
     };
-   
+
     fetchData();
   }, []);
- const handleExcluirUsuario = async (id_pedido) => {
+  useEffect(() => {
+    const fetchData = async () => {
       try {
-        await axios.delete(`http://localhost:3001/pedidos/${id_pedido}`);
-        // Atualiza a lista de cadastros após a exclusão
         const { data } = await axios.get("http://localhost:3001/pedidos");
-        setPedidos(data);
-        console.log("Usuário excluído com sucesso!");
+        console.log("Dados brutos:", data);
+
+        // Convertendo os valores para números
+        const pedidosFormatados = data.map(pedido => ({
+          ...pedido,
+          valorTotal: parseFloat(pedido.valorTotal)
+        }));
+        console.log("Dados formatados:", pedidosFormatados);
+
+        setPedidos(pedidosFormatados);
       } catch (error) {
-        console.error("Erro ao excluir usuário:", error);
+        console.error("Erro ao buscar pedidos:", error);
       }
     };
+
+    fetchData();
+  }, []);
+
+
+  useEffect(() => {
+    const fetchClientes = async () => {
+      const clientesObj = {};
+      slClientes.forEach((cliente) => {
+        clientesObj[cliente.id] = cliente.nome;
+      });
+      setClientes(clientesObj);
+    };
+
+    fetchClientes();
+  }, [slClientes]);
+
+  useEffect(() => {
+    const fetchProdutos = async () => {
+      const produtosObj = {};
+      slProdutos.forEach((produto) => {
+        produtosObj[produto.idProdutos] = produto.nomeProduto;
+      });
+      setProdutos(produtosObj);
+    };
+
+    fetchProdutos();
+  }, [slProdutos]);
+
+  const handleExcluirUsuario = async (id_pedido) => {
+    try {
+      await axios.delete(`http://localhost:3001/pedidos/${id_pedido}`);
+      const { data } = await axios.get("http://localhost:3001/pedidos");
+      setPedidos(data);
+      console.log("Pedido excluído com sucesso!");
+    } catch (error) {
+      console.error("Erro ao excluir pedido:", error);
+    }
+    window.location.reload();
+  };
+
   return (
     <div>
       <select name="id_cliente" id="id_cliente" onChange={handleClienteChange}>
@@ -96,30 +184,49 @@ function Tabela_pedidos() {
         ))}
       </select>
 
+      {quantidadeDisponivel > 0 && (
+        <select name="quantidade" id="quantidade" onChange={handleQuantidadeChange}>
+          <option value="">Selecione a quantidade</option>
+          {Array.from({ length: quantidadeDisponivel }, (_, index) => index + 1).map((quantidade) => (
+            <option key={quantidade} value={quantidade}>
+              {quantidade}
+            </option>
+          ))}
+        </select>
+      )}
+
+      <div>Valor Total: R$ {valorTotal.toFixed(2)}</div>
+
       <button onClick={handleAdicionarPedido}>Adicionar Pedido</button>
 
-      <table>
+      <Table>
         <thead>
           <tr>
             <th>ID</th>
             <th>Cliente</th>
             <th>Produto</th>
+            <th>Quantidade</th>
+            <th>Valor Total</th>
+            <th>Ações</th>
           </tr>
         </thead>
         <tbody>
-  {pedidos.map((pedido) => (
-    <tr key={pedido.id_pedido}>
-      <td>{pedido.id_pedido}</td>
-      <td>{slClientes.find(clientes => clientes.id === pedido.id_cliente)?.nome}</td>
-      <td>{slProdutos.find(produtos => produtos.idProdutos === pedido.id_produto)?.nomeProduto}</td>
-      <td>
-        <Button onClick={() => handleExcluirUsuario(pedido.id_pedido)}>Excluir</Button>
-      </td>
-    </tr>
-  ))}
-</tbody>
+          {pedidos.map((pedido) => (
+            <tr key={pedido.id_pedido}>
+              <td>{pedido.id_pedido}</td>
+              <td>{clientes[pedido.id]}</td>
+              <td>{produtos[pedido.idProdutos]}</td>
+              <td>{pedido.quantidade}</td>
+              <td>{typeof pedido.valorTotal === 'number' ? `R$ ${pedido.valorTotal.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}` : 'Valor Indisponível'}</td>
 
-      </table>
+
+              <td>
+                <Button onClick={() => handleExcluirUsuario(pedido.id_pedido)}>Excluir</Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 }

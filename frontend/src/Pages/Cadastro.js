534c1b9b9c1a3d695751954087f6d0a3374523ef
julia caruso <juliacaruso779@gmail.com>
2024-05-23 15:54:37 -0300

    julia correçao doble tabela

--- frontend/src/Pages/Cadastro.js
+++ frontend/src/Pages/Cadastro.js
@@ -2,19 +2,10 @@
 import React from "react";
 import CadastroForm from "../Components/CadastroForm";
 import TabelaCadastro from "../Components/TabelaCadastro";
-<<<<<<< HEAD
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { useState } from 'react';
 import "../css/tabelacadastrofunc.css";
-=======
-import Rodape from "../Components/Rodape";
-
-
-
-
-
->>>>>>> 3ac0f3a37a592e5a712f62c44ffe7333d77b446f
 
 const Cadastro   = () => {
   const [show, setShow] = useState(false);
@@ -25,29 +16,18 @@ const Cadastro   = () => {
   return (
     <>
     <br/>
-    
     <center>
       <h1>Cadastro dos funcionários</h1>
     </center>
     <br/>
       <CadastroForm />
-<<<<<<< HEAD
-=======
-      <TabelaCadastro/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <Rodape/>
->>>>>>> 3ac0f3a37a592e5a712f62c44ffe7333d77b446f
 
 <Button className="botaotabela" variant="primary" onClick={handleShow}>
-        Tabela Cadastro Funcionarios
+        Tabela Cadastro Funcionários
       </Button>
      <Modal show={show} onHide={handleClose} size="xl">
         <Modal.Header closeButton>
-          <Modal.Title>Tabela Cadastro Funcionarios</Modal.Title>
+          <Modal.Title>Tabela Cadastro Funcionários</Modal.Title>
         </Modal.Header>
         <Modal.Body><TabelaCadastro/> </Modal.Body>
         <Modal.Footer>
